567cbc68aa3a523161d17bf05390d1b5e9d68897
Liubomyr Havrosh <lubgavrosh@gmail.com>
2023-10-11 15:50:30 +0200

    create Entity

---
+++ WebApi/Data/Entitties/CategoryEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Data.Entitties;
+
+namespace WebApi.Data.Entitties
+{
+
+    [Table("tblCategories")]
+    public class CategoryEntity:BaseEntity<int>
+
+    {
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+
+
+        [Required, StringLength(255)]
+        public string Image { get; set; }
+
+
+        [StringLength(4000)] 
+        public string Description { get; set; }
+
+   
+    }
+}
